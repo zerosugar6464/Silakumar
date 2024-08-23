65df338122f0837aea8fe3bdc97b7ca68b2b9de9
Serkandgn81 <doganserkan344@gmail.com>
2024-08-23 09:13:41 +0300

    Update RoseTagger.py

--- RoseTagger.py
+++ RoseTagger.py
@@ -403,7 +403,7 @@ async def chatModeHandler(bot: Client, msg: Message):
     reply = None
 
     if text.startswith("acelya"): # * Mesaj buse ile başlıyorsa cevap veriyoruz
-        reply = random.choice(acelya)
+        reply = random.choice(rose)
         await asyncio.sleep(0.06)
     
     elif kontrol(["selam", "slm", "sa", "selamlar", "selamm"], text): # * Selam yazısı metnin içinde varsa cevap veriyoruz
