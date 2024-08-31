f6dedd754cc41e6b6d6f553eb1a06099953fb52e
Serkandgn81 <doganserkan344@gmail.com>
2024-08-31 20:37:27 +0300

    Update RoseTagger.py

--- RoseTagger.py
+++ RoseTagger.py
@@ -54,7 +54,7 @@ LOGGER = logging.getLogger(__name__)
 
 API_ID = int(os.environ.get("API_ID", "24054192"))
 API_HASH = os.environ.get("API_HASH", "ed9a8a61a1b4a1ad0915cbe87ba490ed")
-BOT_TOKEN = os.environ.get("TOKEN", "7090522217:AAHl4mkav_faEElKDk3KlT4L7EtbdepYyjE")  
+BOT_TOKEN = os.environ.get("TOKEN", "7090522217:AAGhGJl78uuDhOaEnCrR5-jVokpqZ2a844g")  
 
 BOT_ID = int(os.environ.get("BOT_ID", "7090522217"))  
 
