7cd71484f04aa60c22c5f06cf1cf6f68ad5e1277
Silahanim21 <turkcell63632@gmail.com>
2024-11-18 22:29:39 +0300

    Update RoseTagger.py

--- RoseTagger.py
+++ RoseTagger.py
@@ -54,7 +54,7 @@ LOGGER = logging.getLogger(__name__)
 
 API_ID = int(os.environ.get("API_ID", "28217326"))
 API_HASH = os.environ.get("API_HASH", "4800b862b4094e96f42a0b5dc2a558f8")
-BOT_TOKEN = os.environ.get("TOKEN", "8089776460:AAHzk9V4OKGOPaEGxyCZsH1MvdisZC7KPBc")  
+BOT_TOKEN = os.environ.get("TOKEN", "8089776460:AAE7mL7ziDznALl1eMNUpVHWgWRS4HrYloI")  
 
 BOT_ID = int(os.environ.get("BOT_ID", "8089776460"))  
 
